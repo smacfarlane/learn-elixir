02b1ee13be02be5919e97328026b85682f4d5936
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-11-07 21:42:50 -0900

    Add process methods

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -9,7 +9,7 @@ defmodule Issues.CLI do
   """
 
   def run argv do
-    parse_args argv
+    argv |> parse_args |> process
   end
 
   @doc """
@@ -30,5 +30,16 @@ defmodule Issues.CLI do
       { _, [user, project], _ } -> {user, project, @default_count }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage:  issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
