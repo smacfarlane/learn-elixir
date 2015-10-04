c20ed9bf1c037d823e6138f02862e1f8c40d26ab
Scott Macfarlane <scott@gina.alaska.edu>
2015-10-04 14:31:38 -0800

    Fix module name

--- ch14/ex4.exs
+++ ch14/ex4.exs
@@ -2,7 +2,7 @@ defmodule Ch14.Ex4 do
   import :timer, only: [ sleep: 1 ]
   def exit_child(parent) do
     send parent, "Hello"
-    raise :too_soon
+    raise "too_soon"
   end
 
   def receive_messages do
@@ -17,7 +17,7 @@ defmodule Ch14.Ex4 do
 
   def run do
     Process.flag(:trap_exit, true)
-    spawn_link(Ch14.Ex2, :exit_child, [self])
+    spawn_link(Ch14.Ex4, :exit_child, [self])
 
     sleep 500
     receive_messages
