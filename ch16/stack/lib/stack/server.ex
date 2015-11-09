de03a63d5ffe060d5b2d42783e33329e10384e26
Scott Macfarlane <scott@gina.alaska.edu>
2015-11-08 15:41:14 -0900

    Add public api to Stack(Ex: OTP-Servers-3,4,5)

--- ch16/stack/lib/stack/server.ex
+++ ch16/stack/lib/stack/server.ex
@@ -1,11 +1,37 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def start_link initial_list do
+    GenServer.start_link(__MODULE__, initial_list, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push value do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
   def handle_call(:pop, _, [head | tail]) do
     { :reply, head, tail }
   end
 
+  def handle_cast({:push, value}, list) when value < 10 do
+    System.halt(value)
+  end
+
   def handle_cast({:push, value}, list) do
     { :noreply, [value | list]}
+  end
+
+
+  def terminate reason, state do
+    IO.puts "Terminating because #{inspect reason}."
+    IO.puts "Current state: #{inspect state}"
   end
 end
