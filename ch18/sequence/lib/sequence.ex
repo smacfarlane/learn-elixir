3ce20630d6780897abc7a418b185710fdb132ab6
Scott Macfarlane <scott@gina.alaska.edu>
2015-11-09 21:39:57 -0900

    Modify according to book

--- ch18/sequence/lib/sequence.ex
+++ ch18/sequence/lib/sequence.ex
@@ -3,8 +3,8 @@ defmodule Sequence do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, _args) do
-    {:ok, _pid} = Sequence.Supervisor.start_link(123)
+  def start(_type, initial_number) do
+    {:ok, _pid} = Sequence.Supervisor.start_link(Application.get_env(:sequence, :initial_number))
     # import Supervisor.Spec, warn: false
     #
     # children = [
