8620224b87372f9a3e812acb3fb83131b0c72275
Scott Macfarlane <scott@gina.alaska.edu>
2015-11-21 10:53:05 -0900

    Add tests (Ex: OTP-Applications-2)

--- stack/test/stack_test.exs
+++ stack/test/stack_test.exs
@@ -2,7 +2,22 @@ defmodule StackTest do
   use ExUnit.Case
   doctest Stack
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    :sys.replace_state(Stack.Server, fn {_old_state, pid} -> {[1,2,3,4,5], pid} end)
+    :ok
+  end
+
+  test "push adds to the stack" do
+    Stack.Server.push(5)
+    {stack, pid} = :sys.get_state Stack.Server
+    assert stack == [5,1,2,3,4,5]
+  end
+
+  test "pop removes from the stack" do
+    assert Stack.Server.pop == 1
+    assert Stack.Server.pop == 2
+    assert Stack.Server.pop == 3
+    assert Stack.Server.pop == 4
+    assert Stack.Server.pop == 5
   end
 end
